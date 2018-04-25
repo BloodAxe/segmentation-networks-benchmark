5a50ca1d0418cbfe4e353e074e0666e92108a20d
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2018-04-25 20:29:44 +0300

    FIx maxpool by applying asymetric padding

--- lib/torch/seg_net.py
+++ lib/torch/seg_net.py
@@ -6,7 +6,7 @@ from torch.nn import functional as F
 import numpy as np
 
 
-def _squash(p: torch.FloatTensor):
+def _squash(p: torch.Tensor):
     p_norm_sq = torch.sum(p ** 2, dim=2, keepdim=True)
     p_norm = torch.sqrt(p_norm_sq + 1e-9)
     v = p_norm_sq / (1. + p_norm_sq) * p / p_norm
@@ -35,24 +35,19 @@ class CapsuleLayer(nn.Module):
         self.kernel_size = kernel_size
         self.num_capsules = num_capsules
         self.mode = mode
+        self.routing = routing
+        self.one_kernel = torch.ones([1, 1, kernel_size, kernel_size])
 
         if mode == 'conv':
             pad = kernel_size // 2
             self.convOp = nn.Conv2d(input_channels, capsule_dims * num_capsules, kernel_size=kernel_size, stride=stride, padding=pad)
+            self.maxpool = nn.Sequential(nn.ReplicationPad2d(pad), nn.MaxPool2d(kernel_size=kernel_size, stride=1))
 
-            # self.convOp = nn.Sequential(nn.ReplicationPad2d(pad),
-            #                             nn.Conv2d(input_channels, capsule_dims * num_capsules, kernel_size=kernel_size, stride=stride))
-
-            self.maxpool = nn.MaxPool2d(kernel_size=kernel_size, stride=1, padding=pad)
-        else:
-            pad = kernel_size // 2
+        elif mode == 'deconv':
             self.convOp = nn.ConvTranspose2d(input_channels, capsule_dims * num_capsules, kernel_size=kernel_size, stride=stride, padding=1)
-            # self.convOp = nn.Sequential(nn.ReplicationPad2d(pad),
-            #                             nn.ConvTranspose2d(input_channels, capsule_dims * num_capsules, kernel_size=kernel_size, stride=stride))
-            self.maxpool = nn.MaxPool2d(kernel_size=kernel_size, stride=1,padding=1)
-
-        self.one_kernel = Variable(torch.ones([1, 1, kernel_size, kernel_size]))
-        self.routing = routing
+            self.maxpool = nn.Sequential(
+                nn.ZeroPad2d((1, 2, 1, 2)),
+                nn.MaxPool2d(kernel_size=kernel_size, stride=1))
 
     def forward(self, x: torch.autograd.Variable):
         # X is [N, num_capsules, in_capsule_dims, H, W]
@@ -65,24 +60,20 @@ class CapsuleLayer(nn.Module):
         H_0 = x.size(3)
         W_0 = x.size(4)
 
-        u_t_list = [torch.squeeze(u_t, 1) for u_t in torch.chunk(x, t_0, dim=1)]
+        u_t_list = [torch.squeeze(u_t, 1) for u_t in torch.split(x, 1, dim=1)]  # Splits into t_0 chunks
         u_hat_t_list = []
         for u_t in u_t_list:
             # u_t: [N, z_0, H_0, W_0]
-            if self.mode == 'conv':
-                u_hat_t = self.convOp(u_t)
-            else:
-                out_size = [N, t_1 * z_1, H_0 * 2, W_0 * 2]
-                u_hat_t = self.convOp(u_t, output_size=out_size)
-                print(self.mode, x.size(), out_size, u_hat_t.size())
+            u_hat_t = self.convOp(u_t)
+            # print(self.mode, u_t.size(), u_hat_t.size())
 
             H_1 = u_hat_t.size(2)
             W_1 = u_hat_t.size(3)
             u_hat_t = u_hat_t.view([N, t_1, z_1, H_1, W_1])
             u_hat_t_list.append(u_hat_t)
 
-        b = Variable(torch.zeros([N, t_1, t_0, H_1, W_1]))
-        b_t_list = [torch.squeeze(b_t, dim=2) for b_t in torch.chunk(b, t_0, dim=2)]
+        b = torch.zeros([N, t_1, t_0, H_1, W_1])
+        b_t_list = [torch.squeeze(b_t, dim=2) for b_t in torch.split(b, 1, dim=2)]  # Splits into t_0 chunks
         u_hat_t_list_sg = [u_hat_t.detach() for u_hat_t in u_hat_t_list]
 
         for d in range(self.routing):
@@ -96,14 +87,15 @@ class CapsuleLayer(nn.Module):
                 # routing softmax
 
                 b_t_max = self.maxpool(b_t)
+                # print('maxpool', b_t.size(), b_t_max.size())
                 r_t = F.softmax(b_t_max, dim=1)
 
                 # sum_c_t = c_t.sum F.conv2d(c_t, self.one_kernel, stride=1, padding=k // 2)  # [... , 1]
                 # r_t = c_t / sum_c_t  # [N, H_1, W_1, t_1]
 
                 r_t = torch.unsqueeze(r_t, dim=2)  # [N, 1, t_1, H_1, W_1]
-                print('r_t', r_t.size())
-                print('u_hat_t', u_hat_t.size())
+                # print('r_t', r_t.size())
+                # print('u_hat_t', u_hat_t.size())
                 r_t_mul_u_hat_t_list.append(r_t * u_hat_t)  # [N, z_1, t_1, H_1, W_1]
 
             # p = tf.add_n(r_t_mul_u_hat_t_list)  # [N, z_1, t_1, H_1, W_1]
@@ -119,9 +111,9 @@ class CapsuleLayer(nn.Module):
                     # b_t     : [N, t_1, H_1, W_1]
                     # u_hat_t : [N, z_1, t_1, H_1, W_1]
                     # v       : [N, z_1, t_1, H_1, W_1]
-                    print('b_t', b_t.size())
-                    print('u_hat_t', u_hat_t.size())
-                    print('v', v.size())
+                    # print('b_t', b_t.size())
+                    # print('u_hat_t', u_hat_t.size())
+                    # print('v', v.size())
 
                     b_t_list_.append(b_t + torch.sum(u_hat_t * v, dim=2, keepdim=False))
                 b_t_list = b_t_list_
@@ -134,6 +126,7 @@ class SegCaps(nn.Module):
     https://arxiv.org/pdf/1804.04241.pdf
     https://github.com/iwyoo/tf-SegCaps/blob/master/model.py
     """
+
     def __init__(self, num_classes=1, input_channels=3):
         super(SegCaps, self).__init__()
 
@@ -149,13 +142,21 @@ class SegCaps(nn.Module):
                                   CapsuleLayer('conv', input_channels=64, capsule_dims=32, kernel_size=5, stride=1, num_capsules=8, routing=3))
 
         self.dec31 = CapsuleLayer('deconv', input_channels=32, capsule_dims=32, kernel_size=4, stride=2, num_capsules=8, routing=3)
+        # self.dec31 = CapsuleLayer('up', input_channels=32, capsule_dims=32, kernel_size=5, stride=1, num_capsules=8, routing=3)
         self.dec32 = CapsuleLayer('conv', input_channels=32, capsule_dims=32, kernel_size=5, stride=1, num_capsules=4, routing=3)
 
         self.dec21 = CapsuleLayer('deconv', input_channels=32, capsule_dims=16, kernel_size=4, stride=2, num_capsules=4, routing=3)
+        # self.dec21 = CapsuleLayer('up', input_channels=32, capsule_dims=16, kernel_size=5, stride=1, num_capsules=4, routing=3)
         self.dec22 = CapsuleLayer('conv', input_channels=16, capsule_dims=16, kernel_size=5, stride=1, num_capsules=4, routing=3)
 
         self.dec11 = CapsuleLayer('deconv', input_channels=16, capsule_dims=16, kernel_size=4, stride=2, num_capsules=2, routing=3)
+        # self.dec11 = CapsuleLayer('up', input_channels=16, capsule_dims=16, kernel_size=5, stride=1, num_capsules=2, routing=3)
         self.dec12 = CapsuleLayer('conv', input_channels=16, capsule_dims=16, kernel_size=1, stride=1, num_capsules=1, routing=3)
+
+        self.reconstruction = nn.Sequential(
+            nn.Conv2d(16, 64, 1),
+            nn.Conv2d(64, 128, 1),
+            nn.Conv2d(128, self.input_channels, 1))
 
     def forward(self, x):
         x = self.firstconv(x)
@@ -189,15 +190,13 @@ class SegCaps(nn.Module):
         x = torch.cat([x, skip1], dim=1)
         x = self.dec12(x)
 
-        x = x.squeeze(x, axis=1)
+        x = torch.squeeze(x, dim=1)
 
         # 1. compute length of vector
-        v_lens = self.compute_vector_length(x)
+        v_lens = _compute_vector_length(x)
 
         # 2. Get masked reconstruction
-        x = self.conv2d(x, 64, 1)
-        x = self.conv2d(x, 128, 1)
-        recons = self.conv2d(x, self.input_channels, 1)
+        recons = self.reconstruction(x)
 
         # if self.num_classes > 1:
         #     x = F.log_softmax(x, dim=1)
@@ -205,9 +204,31 @@ class SegCaps(nn.Module):
         return v_lens, recons
 
 
+def reconstruction_loss(input, target):
+    # recon_loss = tf.reduce_mean(tf.square((images - recons)))
+    return F.mse_loss(input, target)
+
+
+def class_loss(input, target):
+    # class_loss = tf.reduce_mean(
+    #   labels * tf.square(tf.maximum(0., 0.9 - v_lens)) +
+    #     0.5 * (1. - labels) * tf.square(tf.maximum(0., v_lens - 0.1)))
+
+    class_loss = tf.reduce_mean(
+        labels * tf.square(tf.maximum(0., 0.9 - v_lens)) +
+        0.5 * (1. - labels) * tf.square(tf.maximum(0., v_lens - 0.1)))
+
+
+def compute_loss(v_lens, recons, images, labels):
+    total_loss = class_loss(v_lens, labels) + 0.0005 * reconstruction_loss(recons, images)
+    return total_loss
+
+
 x = np.random.rand(1, 3, 512, 512)
 x = torch.from_numpy(x.astype(np.float32))
 
 model = SegCaps(num_classes=1, input_channels=3)
 model.eval()
-y, r = model.forward(Variable(x))
+y, r = model.forward(x)
+print(y.size())
+print(r.size())
