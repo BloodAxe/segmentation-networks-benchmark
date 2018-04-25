60de604c53d14b69efcc62ee707f0431314be71b
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2018-04-25 23:47:04 +0300

    Added factorized unet

--- lib/torch/unet.py
+++ lib/torch/unet.py
@@ -95,3 +95,91 @@ class UNet(nn.Module):
             if self.num_classes > 1:
                 x_out = F.log_softmax(x_out, dim=1)
         return x_out
+
+
+def factorized_conv3x3(in_, out):
+    return nn.Sequential(nn.Conv2d(in_, out, (3, 1), padding=(1, 0)), nn.Conv2d(out, out, (1, 3), padding=(0, 1)))
+
+
+class _FactorizedConv3BN(nn.Module):
+    def __init__(self, in_: int, out: int, bn=False):
+        super().__init__()
+        self.conv = factorized_conv3x3(in_, out)
+        self.bn = nn.BatchNorm2d(out) if bn else None
+        self.activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        x = self.activation(x)
+        return x
+
+
+class _FactorizedUNetModule(nn.Module):
+    def __init__(self, in_: int, out: int):
+        super().__init__()
+        self.l1 = _FactorizedConv3BN(in_, out)
+        self.l2 = _FactorizedConv3BN(out, out)
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.l2(x)
+        return x
+
+
+class FactorizedUNet(nn.Module):
+    output_downscaled = 1
+    module = _FactorizedUNetModule
+
+    def __init__(self,
+                 input_channels: int = 3,
+                 filters_base: int = 32,
+                 down_filter_factors=(1, 2, 4, 8, 16),
+                 up_filter_factors=(1, 2, 4, 8, 16),
+                 bottom_s=4,
+                 num_classes=1,
+                 add_output=True):
+        super(FactorizedUNet, self).__init__()
+        self.num_classes = num_classes
+        assert len(down_filter_factors) == len(up_filter_factors)
+        assert down_filter_factors[-1] == up_filter_factors[-1]
+        down_filter_sizes = [filters_base * s for s in down_filter_factors]
+        up_filter_sizes = [filters_base * s for s in up_filter_factors]
+        self.down, self.up = nn.ModuleList(), nn.ModuleList()
+        self.down.append(self.module(input_channels, down_filter_sizes[0]))
+        for prev_i, nf in enumerate(down_filter_sizes[1:]):
+            self.down.append(self.module(down_filter_sizes[prev_i], nf))
+        for prev_i, nf in enumerate(up_filter_sizes[1:]):
+            self.up.append(self.module(
+                down_filter_sizes[prev_i] + nf, up_filter_sizes[prev_i]))
+        pool = nn.MaxPool2d(2, 2)
+        pool_bottom = nn.MaxPool2d(bottom_s, bottom_s)
+        upsample = nn.Upsample(scale_factor=2)
+        upsample_bottom = nn.Upsample(scale_factor=bottom_s)
+        self.downsamplers = [None] + [pool] * (len(self.down) - 1)
+        self.downsamplers[-1] = pool_bottom
+        self.upsamplers = [upsample] * len(self.up)
+        self.upsamplers[-1] = upsample_bottom
+        self.add_output = add_output
+        if add_output:
+            self.conv_final = nn.Conv2d(up_filter_sizes[0], num_classes, 1)
+
+    def forward(self, x):
+        xs = []
+        for downsample, down in zip(self.downsamplers, self.down):
+            x_in = x if downsample is None else downsample(xs[-1])
+            x_out = down(x_in)
+            xs.append(x_out)
+
+        x_out = xs[-1]
+        for x_skip, upsample, up in reversed(
+                list(zip(xs[:-1], self.upsamplers, self.up))):
+            x_out = upsample(x_out)
+            x_out = up(torch.cat([x_out, x_skip], 1))
+
+        if self.add_output:
+            x_out = self.conv_final(x_out)
+            if self.num_classes > 1:
+                x_out = F.log_softmax(x_out, dim=1)
+        return x_out
