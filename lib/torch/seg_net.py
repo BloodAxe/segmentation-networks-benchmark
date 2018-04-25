dae892465bc228720ca14164e3f5971668f29265
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2018-04-25 23:39:44 +0300

    Implement routing softmax

--- lib/torch/seg_net.py
+++ lib/torch/seg_net.py
@@ -1,9 +1,13 @@
+import cv2
+import random
+
 import torch
 from torch import nn
-from torch.autograd import Variable
-from torchvision import models
 from torch.nn import functional as F
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+from lib.torch.common import to_float_tensor, show_landmarks_batch
 
 
 def _squash(p: torch.Tensor):
@@ -14,7 +18,32 @@ def _squash(p: torch.Tensor):
 
 
 def _compute_vector_length(x):
-    return torch.sqrt(torch.sum(x * x, dim=-1, keepdim=True) + 1e-9)
+    return torch.sqrt(torch.sum(x * x, dim=1, keepdim=True) + 1e-9)
+
+
+class RoutingSoftmax(nn.Module):
+    def __init__(self, kernel_size, t):
+        super(RoutingSoftmax, self).__init__()
+        self.kernel_size = kernel_size
+
+        if kernel_size % 2 == 0:
+            self.padding = nn.ZeroPad2d((1, 2, 1, 2))
+        else:
+            self.padding = nn.ZeroPad2d(kernel_size // 2)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=kernel_size, stride=1, padding=0)
+        self.one_kernel = torch.ones([1, t, kernel_size, kernel_size])
+
+    def forward(self, b_t):
+        b_t_pad = self.padding(b_t)
+        b_t_max = self.maxpool(b_t_pad)
+
+        b_t_max, _ = torch.max(b_t_max, dim=2, keepdim=True)
+        c_t = torch.exp(b_t - b_t_max)  # [N, t_1, H_1, W_1]
+        c_t_pad = self.padding(c_t)
+        sum_c_t = F.conv2d(c_t_pad, self.one_kernel, stride=1, padding=0)
+        r_t = c_t / sum_c_t  # [N, t_1, H_1, W_1]
+        return r_t
 
 
 class CapsuleLayer(nn.Module):
@@ -36,18 +65,13 @@ class CapsuleLayer(nn.Module):
         self.num_capsules = num_capsules
         self.mode = mode
         self.routing = routing
-        self.one_kernel = torch.ones([1, 1, kernel_size, kernel_size])
+        self.routing_softmax = RoutingSoftmax(kernel_size, num_capsules)
 
         if mode == 'conv':
             pad = kernel_size // 2
             self.convOp = nn.Conv2d(input_channels, capsule_dims * num_capsules, kernel_size=kernel_size, stride=stride, padding=pad)
-            self.maxpool = nn.Sequential(nn.ReplicationPad2d(pad), nn.MaxPool2d(kernel_size=kernel_size, stride=1))
-
         elif mode == 'deconv':
             self.convOp = nn.ConvTranspose2d(input_channels, capsule_dims * num_capsules, kernel_size=kernel_size, stride=stride, padding=1)
-            self.maxpool = nn.Sequential(
-                nn.ZeroPad2d((1, 2, 1, 2)),
-                nn.MaxPool2d(kernel_size=kernel_size, stride=1))
 
     def forward(self, x: torch.autograd.Variable):
         # X is [N, num_capsules, in_capsule_dims, H, W]
@@ -85,15 +109,9 @@ class CapsuleLayer(nn.Module):
             r_t_mul_u_hat_t_list = []
             for b_t, u_hat_t in zip(b_t_list, u_hat_t_list_):
                 # routing softmax
-
-                b_t_max = self.maxpool(b_t)
-                # print('maxpool', b_t.size(), b_t_max.size())
-                r_t = F.softmax(b_t_max, dim=1)
-
-                # sum_c_t = c_t.sum F.conv2d(c_t, self.one_kernel, stride=1, padding=k // 2)  # [... , 1]
-                # r_t = c_t / sum_c_t  # [N, H_1, W_1, t_1]
-
+                r_t = self.routing_softmax(b_t)
                 r_t = torch.unsqueeze(r_t, dim=2)  # [N, 1, t_1, H_1, W_1]
+
                 # print('r_t', r_t.size())
                 # print('u_hat_t', u_hat_t.size())
                 r_t_mul_u_hat_t_list.append(r_t * u_hat_t)  # [N, z_1, t_1, H_1, W_1]
@@ -142,15 +160,12 @@ class SegCaps(nn.Module):
                                   CapsuleLayer('conv', input_channels=64, capsule_dims=32, kernel_size=5, stride=1, num_capsules=8, routing=3))
 
         self.dec31 = CapsuleLayer('deconv', input_channels=32, capsule_dims=32, kernel_size=4, stride=2, num_capsules=8, routing=3)
-        # self.dec31 = CapsuleLayer('up', input_channels=32, capsule_dims=32, kernel_size=5, stride=1, num_capsules=8, routing=3)
         self.dec32 = CapsuleLayer('conv', input_channels=32, capsule_dims=32, kernel_size=5, stride=1, num_capsules=4, routing=3)
 
         self.dec21 = CapsuleLayer('deconv', input_channels=32, capsule_dims=16, kernel_size=4, stride=2, num_capsules=4, routing=3)
-        # self.dec21 = CapsuleLayer('up', input_channels=32, capsule_dims=16, kernel_size=5, stride=1, num_capsules=4, routing=3)
         self.dec22 = CapsuleLayer('conv', input_channels=16, capsule_dims=16, kernel_size=5, stride=1, num_capsules=4, routing=3)
 
         self.dec11 = CapsuleLayer('deconv', input_channels=16, capsule_dims=16, kernel_size=4, stride=2, num_capsules=2, routing=3)
-        # self.dec11 = CapsuleLayer('up', input_channels=16, capsule_dims=16, kernel_size=5, stride=1, num_capsules=2, routing=3)
         self.dec12 = CapsuleLayer('conv', input_channels=16, capsule_dims=16, kernel_size=1, stride=1, num_capsules=1, routing=3)
 
         self.reconstruction = nn.Sequential(
@@ -204,31 +219,107 @@ class SegCaps(nn.Module):
         return v_lens, recons
 
 
-def reconstruction_loss(input, target):
-    # recon_loss = tf.reduce_mean(tf.square((images - recons)))
-    return F.mse_loss(input, target)
+class SegCapsLoss:
+    """
+    Loss defined as class_loss + reconstruction_loss
+    """
+
+    def __init__(self, reconstruction_weight=0.0005):
+        self.reconstruction_weight = reconstruction_weight
+
+    def _reconstruction_loss(self, outputs, targets):
+        return F.mse_loss(outputs, targets)
+
+    def _class_loss(self, outputs, targets):
+        zero_input = torch.zeros_like(outputs)
+        a = torch.max(zero_input, 0.9 - outputs)
+        b = torch.max(zero_input, outputs - 0.1)
+        x = 0.5 * targets * (a ** 2) + 0.5 * (1 - targets) * (b ** 2)
+        return x.mean()
+
+    def __call__(self, outputs, targets):
+        v_lens, recons = outputs
+        v_lens_true, recons_true = y
+        loss1 = self._class_loss(v_lens, v_lens_true)
+        loss2 = self._reconstruction_loss(recons, recons_true) * self.reconstruction_weight
+        return loss1 + loss2
 
 
-def class_loss(input, target):
-    # class_loss = tf.reduce_mean(
-    #   labels * tf.square(tf.maximum(0., 0.9 - v_lens)) +
-    #     0.5 * (1. - labels) * tf.square(tf.maximum(0., v_lens - 0.1)))
+def gen_random_image(size):
+    img = np.zeros((size, size, 3), dtype=np.uint8)
+    mask = np.zeros((size, size, 1), dtype=np.uint8)
 
-    class_loss = tf.reduce_mean(
-        labels * tf.square(tf.maximum(0., 0.9 - v_lens)) +
-        0.5 * (1. - labels) * tf.square(tf.maximum(0., v_lens - 0.1)))
+    # Background
+    dark_color0 = random.randint(0, 100)
+    dark_color1 = random.randint(0, 100)
+    dark_color2 = random.randint(0, 100)
+    img[:, :, 0] = dark_color0
+    img[:, :, 1] = dark_color1
+    img[:, :, 2] = dark_color2
+
+    # Object
+    light_color0 = random.randint(dark_color0 + 1, 255)
+    light_color1 = random.randint(dark_color1 + 1, 255)
+    light_color2 = random.randint(dark_color2 + 1, 255)
+    center_0 = random.randint(0, size)
+    center_1 = random.randint(0, size)
+    r1 = random.randint(10, 56)
+    r2 = random.randint(10, 56)
+    cv2.ellipse(img, (center_0, center_1), (r1, r2), 0, 0, 360, (light_color0, light_color1, light_color2), -1)
+    cv2.ellipse(mask, (center_0, center_1), (r1, r2), 0, 0, 360, 255, -1)
+
+    # White noise
+    density = random.uniform(0, 0.1)
+    for i in range(size):
+        for j in range(size):
+            if random.random() < density:
+                img[i, j, 0] = random.randint(0, 255)
+                img[i, j, 1] = random.randint(0, 255)
+                img[i, j, 2] = random.randint(0, 255)
+
+    return img, mask
 
 
-def compute_loss(v_lens, recons, images, labels):
-    total_loss = class_loss(v_lens, labels) + 0.0005 * reconstruction_loss(recons, images)
-    return total_loss
+class SyntheticShapes(Dataset):
+
+    def __init__(self, size):
+        self.size = size
+
+    def __getitem__(self, index):
+        img, mask = gen_random_image(self.size)
+        return to_float_tensor(img), to_float_tensor(mask)
+
+    def __len__(self):
+        return 1024
 
 
-x = np.random.rand(1, 3, 512, 512)
-x = torch.from_numpy(x.astype(np.float32))
+if __name__ == "__main__":
 
-model = SegCaps(num_classes=1, input_channels=3)
-model.eval()
-y, r = model.forward(x)
-print(y.size())
-print(r.size())
+    trainloader = DataLoader(SyntheticShapes(512), batch_size=3)
+    test_x, test_y = next(iter(trainloader))
+
+    show_landmarks_batch((test_x, test_y))
+
+    model = SegCaps(num_classes=1, input_channels=3)
+    model.train()
+    criterion = SegCapsLoss()
+
+    optim = torch.optim.SGD(model.parameters(), lr=0.001)
+
+    for epoch in range(10):
+
+        for i, (x, y) in enumerate(trainloader):
+            # x, y = x.cuda(), y.cuda()
+
+            # zero the parameter gradients
+            optim.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(x)
+            loss = criterion(outputs, y)
+
+            bs = x.size(0)
+            (bs * loss).backward()
+
+            optim.step()
+            print(epoch, i, loss)
