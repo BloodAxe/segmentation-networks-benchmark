74db49418810d1614b0b4a25ad002d794c4e6bdf
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2018-06-18 16:10:34 +0300

    Add PR curve meter

--- lib/train_utils.py
+++ lib/train_utils.py
@@ -2,6 +2,7 @@ import glob
 import os
 
 import torch
+from sklearn.metrics import confusion_matrix
 
 from torch import nn
 from torch.optim import Optimizer, SGD
@@ -33,7 +34,6 @@ class AverageMeter(object):
 
 
 def find_optimal_lr(model: nn.Module, criterion, optimizer: Optimizer, dataloader):
-
     min_lr = 1e-8
     lrs = []
     lr = min_lr
@@ -87,3 +87,45 @@ def auto_file(filename, where='.') -> str:
         raise FileNotFoundError('More than one file matches given filename. Please specify it explicitly' + filename)
 
     return files[0]
+
+
+class PRCurveMeter(object):
+
+    def __init__(self, thresholds=127):
+        self.reset(np.arange(0., 1., 1. / thresholds,dtype=np.float32))
+        self.precision = None
+        self.recall = None
+
+    def reset(self, thresholds):
+        self.thresholds = thresholds
+        n_thresholds = len(thresholds)
+
+        self.tp = np.zeros(n_thresholds, dtype=int)
+        self.tn = np.zeros(n_thresholds, dtype=int)
+        self.fp = np.zeros(n_thresholds, dtype=int)
+        self.fn = np.zeros(n_thresholds, dtype=int)
+        self.precision = np.zeros(n_thresholds, dtype=int)
+        self.recall = np.zeros(n_thresholds, dtype=int)
+
+    def compute_stat(self, y_true, y_pred):
+        cm = confusion_matrix(y_true.flatten(), y_pred.flatten())
+
+        tn = cm[0][0]
+        fn = cm[1][0]
+        tp = cm[1][1]
+        fp = cm[0][1]
+
+        return tp, tn, fp, fn
+
+    def update(self, y_true, y_pred):
+        y_true = y_true.astype(np.bool)
+        for i, value in enumerate(self.thresholds):
+            tp, tn, fp, fn = self.compute_stat(y_true, y_pred > value)
+
+            self.tp[i] += tp
+            self.tn[i] += tn
+            self.fp[i] += fp
+            self.fn[i] += fn
+
+        self.precision = self.tp.astype(np.float32) / (self.tp + self.fp)
+        self.recall = self.tp.astype(np.float32) / (self.tp + self.fn)
